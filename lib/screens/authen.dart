01b01521542f1015915651c9823d61c7c02da23c
masterUNG <phrombutr@gmail.com>
2019-06-12 10:38:23 +0700

    design UI Authen finish

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -8,9 +8,47 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   // Explcit
   double amount = 150.0;
-  double size = 200.0;
+  double size = 250.0;
 
   // Method
+
+  Widget mySizeBox() {
+    return SizedBox(
+      width: 4.0,
+      height: 15.0,
+    );
+  }
+
+  Widget signInButton() {
+    return Expanded(
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.blue[700],
+        child: Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        borderSide: BorderSide(color: Colors.blue[700]),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.blue[700]),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
 
   Widget emailTextFormField() {
     return Container(
@@ -19,9 +57,9 @@ class _AuthenState extends State<Authen> {
         width: size,
         child: TextFormField(
           decoration: InputDecoration(
-            labelText: 'Email :',
-            hintText: 'you@email.com',
-          ),
+              labelText: 'Email :',
+              hintText: 'you@email.com',
+              labelStyle: TextStyle(color: Colors.blue[800])),
         ),
       ),
     );
@@ -34,9 +72,9 @@ class _AuthenState extends State<Authen> {
         width: size,
         child: TextFormField(
           decoration: InputDecoration(
-            labelText: 'Password :',
-            hintText: 'More 6 Charactor',
-          ),
+              labelText: 'Password :',
+              hintText: 'More 6 Charactor',
+              labelStyle: TextStyle(color: Colors.blue[800])),
         ),
       ),
     );
@@ -52,7 +90,6 @@ class _AuthenState extends State<Authen> {
 
   Widget showName() {
     return Container(
-      margin: EdgeInsets.only(top: 15.0),
       child: Text(
         'Mush Room IoT',
         style: TextStyle(
@@ -66,16 +103,42 @@ class _AuthenState extends State<Authen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(resizeToAvoidBottomPadding: false,
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment(0, 0),
+            colors: [
+              Colors.white,
+              Colors.blue[700],
+            ],
+            radius: 1.5,
+          ),
+        ),
         padding: EdgeInsets.only(top: 70.0),
         alignment: Alignment(0, -1),
         child: Column(
           children: <Widget>[
             showLogo(),
+            mySizeBox(),
             showName(),
             emailTextFormField(),
             passwordTextFormField(),
+            mySizeBox(),
+            Container(
+              alignment: Alignment.center,
+              child: Container(
+                width: size,
+                child: Row(
+                  children: <Widget>[
+                    signInButton(),
+                    mySizeBox(),
+                    signUpButton(),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
