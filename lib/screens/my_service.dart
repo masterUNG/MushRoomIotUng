22a9c7318bb2826199e53d0708b221a43f3b72a5
masterUNG <phrombutr@gmail.com>
2019-06-13 15:01:51 +0700

    Get database firebase ok

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'dart:io';
 
 class MyService extends StatefulWidget {
@@ -10,8 +11,62 @@ class MyService extends StatefulWidget {
 class _MyServiceState extends State<MyService> {
   // Explicit
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  FirebaseDatabase firebaseDatabase = FirebaseDatabase.instance;
+  String nameLogin = "", uidString;
 
   // Method
+  @override
+  void initState() {
+    super.initState();
+    findUidLogin();
+  }
+
+  void findUidLogin() async {
+    await firebaseAuth.currentUser().then((objValue) {
+      uidString = objValue.uid.toString().trim();
+      print('uidString ==> $uidString');
+      findNameLogin();
+    });
+  }
+
+  void findNameLogin() async {
+    await firebaseDatabase
+        .reference()
+        .child('User')
+        .child(uidString)
+        .once()
+        .then((DataSnapshot dataSnapshop) {
+      String response = dataSnapshop.value.toString();
+      print('response = $response');
+
+      Map<dynamic, dynamic> map = dataSnapshop.value;
+      setState(() {
+        nameLogin = map['Name'];
+      });
+      print('nameLogin ==> $nameLogin');
+    });
+  }
+
+  Widget showTitle() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text('My Service'),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Login by $nameLogin',
+              style: TextStyle(fontSize: 15.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget signOutButton() {
     return IconButton(
       icon: Icon(Icons.exit_to_app),
@@ -33,7 +88,7 @@ class _MyServiceState extends State<MyService> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyService'),
+        title: showTitle(),
         actions: <Widget>[signOutButton()],
       ),
       body: Text('body'),
